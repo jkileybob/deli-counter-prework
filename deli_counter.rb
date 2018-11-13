64d16b87c27f41760f222c64915109e6d0d23271
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 23:31:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,14 @@
 # Write your code here.
 katz_deli = []
+counter = 0
+def line(katz_deli)
+  if katz_deli.size > 0
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  else
+    puts "The line is empty."  
+  end  
+end
+
+def take_a_number(katz_deli, name)
+  counter = 0
+    
